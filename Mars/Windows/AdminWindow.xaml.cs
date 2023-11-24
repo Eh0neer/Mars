c7c38726be3665376075b5e03ca87a0b04def790
FeerSSQ <114866823+FeerSSQ@users.noreply.github.com>
2023-11-24 22:40:57 +0700

    add a new window

---
+++ Mars/Windows/AdminWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows;
+
+
+namespace Mars
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowEmployees(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowOrders(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowShifts(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowReports(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowMenu(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteEmployee(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateEmployee(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EditEmployee(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Cancellation(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RefreshList(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mars/Windows/AuthWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace Mars.Windows;
+
+public partial class AuthWindow : Window
+{
+    public AuthWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void LoginButtonClick(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+    private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+    {
+        LoginPlaceholder.Visibility = Visibility.Collapsed;
+    }
+
+    private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(txtUsername.Text))
+        {
+            LoginPlaceholder.Visibility = Visibility.Visible;
+        }
+    }
+
+    private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+    {
+        PasswordPlaceholder.Visibility = Visibility.Collapsed;
+    }
+
+    private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(txtPassword.Password))
+        {
+            PasswordPlaceholder.Visibility = Visibility.Visible;
+        }
+    }
+
+}
