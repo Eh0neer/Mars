04750198c437cd5c71b708f2d1eb7c3a7c26c157
FeerSSQ <114866823+FeerSSQ@users.noreply.github.com>
2023-11-26 15:10:16 +0700

    add new waiter window

---
+++ Mars/Windows/WaiterWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+
+namespace Mars.Windows;
+
+public partial class WaiterWindow : Window
+{
+    public WaiterWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ChangeOrderStatus(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void CreateOrder(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void ShowOrders(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void ShowReports(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void CashPayment(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void NonCashPayment(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void RefreshList(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
